69c592314b86368730828020df1b23169256e224
Pawel Szalawinski <pawel.szalawinski@tink.com>
2021-04-25 14:02:32 +0200

    added listing files

--- screenShooter.py
+++ screenShooter.py
@@ -4,11 +4,20 @@ import shutil
 import time
 import tkinter as tk
 from tkinter import filedialog
+import pynput.mouse    as ms
 import pynput.keyboard as kb
 import os
+from os import listdir
 import pyautogui
 
 keyboard = kb.Controller()
+mouse = ms.Controller()
+
+print("Keys:\n cmd - takes screenshot\n shift - end program\n space - list all files")
+
+def keyShooter(x, y, button, pressed):
+    screenshooter()
+        
 
 def screenshooter():
     t = time.localtime()
@@ -16,13 +25,17 @@ def screenshooter():
     file_name = ("screen-" + timestamp + ".png")
     path = r"/Users/pawel.szalawinski/Pictures/Shoots/"
     savepath = path + file_name
-
     myScreenshot = pyautogui.screenshot()
     myScreenshot.save(savepath)
+
+def printDirectory():
+    dirList = os.listdir('/Users/pawel.szalawinski/Pictures/Shoots/')
+    print(dirList)
 
 def on_press(key):
     if key == kb.Key.shift :os._exit(0) 
     if key == kb.Key.cmd: screenshooter()
+    if key == kb.Key.space: printDirectory()
 
 with kb.Listener(
         on_press=on_press) as listener:
