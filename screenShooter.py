f67c19836337474748e3ba1f1589e9e43f49e400
Pawel Szalawinski <pawel.szalawinski@tink.com>
2021-04-25 13:32:49 +0200

    working shooter with keyboard

--- screenShooter.py
+++ screenShooter.py
@@ -4,49 +4,26 @@ import shutil
 import time
 import tkinter as tk
 from tkinter import filedialog
-from pynput.mouse import Button, Controller, Listener
+import pynput.keyboard as kb
 import os
-
 import pyautogui
 
-# root = tk.Tk()
-
-# canvas1 = tk.Canvas(root, width=200, height=50)
-# canvas1.pack()
-mouse = Controller()
-
-
-def on_click(x, y, button, pressed):
-    screenshooter()
-
-def on_scroll(x, y, dx, dy):
-    os._exit(0)
-
-# def takeScreenshot():
-#     while True:
-#     screenshooter()
+keyboard = kb.Controller()
 
 def screenshooter():
     t = time.localtime()
     timestamp = time.strftime('%b-%d-%Y_%H%M%S', t)
     file_name = ("screen-" + timestamp + ".png")
-    path = r"C:\Users\Szau\Pictures\\Screen"
+    path = r"/Users/pawel.szalawinski/Pictures/Shoots/"
     savepath = path + file_name
 
     myScreenshot = pyautogui.screenshot()
-    #file_path = filedialog.SaveAs(defaultextension='.jpg')
-    #file_path = filedialog.asksaveasfilename(defaultextension='.png')
     myScreenshot.save(savepath)
-    # time.sleep(1.5)
 
+def on_press(key):
+    if key == kb.Key.shift :os._exit(0) 
+    if key == kb.Key.cmd: screenshooter()
 
-# myButton = tk.Button(text='Take Screenshot',
-#                      command=takeScreenshot, bg='green', fg='white', font=10)
-# canvas1.create_window(100, 25, window=myButton)
-# root.mainloop()
-
-with Listener(
-    on_click=on_click,
-    on_scroll=on_scroll
-) as listener:
+with kb.Listener(
+        on_press=on_press) as listener:
     listener.join()
