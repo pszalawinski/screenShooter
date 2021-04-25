6a7e17c0d417bdadede0b94fee740e8fc8b72b24
Pawel Szalawinski <pawel.szalawinski@tink.com>
2021-04-25 11:09:10 +0200

    working saving option

---
+++ go.mod
@@ -0,0 +1,9 @@
+module main
+
+go 1.16
+
+require (
+	github.com/google/renameio v0.1.0 // indirect
+	github.com/kbinani/screenshot v0.0.0-20210326165202-b96eb3309bb0
+	golang.org/x/tools/gopls v0.6.10 // indirect
+)
--- main.go
+++ main.go
@@ -5,8 +5,8 @@ import (
 	"fmt"
 	"image"
 	"image/png"
-
 	"os"
+	"time"
 
 	"github.com/kbinani/screenshot"
 )
@@ -19,22 +19,19 @@ func main() {
 	scanner.Scan()
 
 	var inputPath string = scanner.Text()
-	if inputPath == "1" {
-		captureDisplay()
+
+	for inputPath != "3" {
+		if inputPath == "1" {
+			captureDisplay(1)
+		}
+		if inputPath == "2" {
+			captureDisplay(2)
+		}
 	}
 
 }
 
-func printerOne() {
-	fmt.Println("kliknieto jeden")
-}
-
-func printerTwo() {
-	fmt.Println("kliknieto dwa")
-}
-
-func captureDisplay() {
-	var i int = 1
+func captureDisplay(i int) {
 
 	bounds := screenshot.GetDisplayBounds(i)
 
@@ -42,9 +39,9 @@ func captureDisplay() {
 	if err != nil {
 		panic(err)
 	}
-	fileName := fmt.Sprintf("%d_%dx%d.png", i, bounds.Dx(), bounds.Dy())
+	t := time.Now().Second()
+	fileName := fmt.Sprintf("%d_time-%d.png", i, t)
 	save(img, fileName)
-	fmt.Printf("#%d : %v \"%s\"\n", i, bounds, fileName)
 }
 
 func save(img *image.RGBA, filePath string) {
