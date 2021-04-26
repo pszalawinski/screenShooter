b6610b159c6247c910a2f3284b9cb1206e68f70b
Pawel Szalawinski <pawel.szalawinski@tink.com>
2021-04-26 23:23:53 +0200

    created structure

--- screenShooter.py
+++ screenShooter.py
@@ -13,6 +13,7 @@ from fpdf import FPDF
 import glob
 from PIL import Image
 import shutil
+from utils import sleeper
 
 keyboard = kb.Controller()
 # mouse = ms.Controller()
@@ -21,25 +22,52 @@ path = '/Users/pawel.szalawinski/Pictures/Shoots/'
 
 print("Keys:\n cmd - takes screenshot\n esc - end program\n shift - list all files")
 
+path = input("provide path where operations should be made: ")
+sleeper.printDots()
+
+class Counter:
+    def __init__(self, x, y):
+        self._x = x
+        self._y = y
+
+    def get_x(self):
+        return self._x
+
+    def set_x(self, x):
+        self._x = x
+
+    def get_y(self):
+        return self._y
+
+    def set_y(self, y):
+        self._y = y
+
+counter = Counter(0, path)
+print("Ready to taking shots!")
+
 def keyShooter(x, y, button, pressed):
     screenshooter()
-        
 
-def screenshooter():
-    t = time.localtime()
-    timestamp = time.strftime('%b-%d-%Y_%H%M%S', t)
-    file_name = ("screen-" + timestamp + ".png")
-    path = r"/Users/pawel.szalawinski/Pictures/Shoots/"
+def addCounter(obj):
+    counter = obj.get_x()
+    counter2 = counter + 1
+    obj.set_x(counter2)
+
+def screenshooter(counter):
+    # t = time.localtime()
+    # timestamp = time.strftime('%b-%d-%Y_%H%M%S', t)
+    click = counter.get_x()
+    # /Users/pawel.szalawinski/Pictures/Shoots/
+    file_name = (str(click) + ".png")
+    path = counter.get_y()
     savepath = path + file_name
     myScreenshot = pyautogui.screenshot()
     myScreenshot.save(savepath)
+    addCounter(counter)
 
 def printDirectory(path):
     dirList = os.listdir(path)
     print(dirList)
-
-def listdir_nohidden(path):
-    print(glob.glob(os.path.join(path, '*')))
 
 def getlistdir_nohidden(path):
     imageList = glob.glob(os.path.join(path, '*'))
@@ -53,18 +81,21 @@ def getlistdir_nohidden(path):
     print("PDF file created")
 
 
-def createPdfFile():
-    temp_dir = "/Users/pawel.szalawinski/Pictures/Shoots/tmp/"
-    dest_dir = "/Users/pawel.szalawinski/Pictures/Shoots/readyPDF/"
+def createPdfFile(counter):
+
+    path = counter.get_y()
+
+    temp_dir = path + "tmp/"
+    dest_dir = path + "readyPDF/"
 
     os.mkdir(temp_dir)
     os.mkdir(dest_dir)
 
-    counter = 1
-    png_path = "/Users/pawel.szalawinski/Pictures/Shoots/screen-Apr-25-2021_141913.png"
-    jpg_filename = temp_dir + "jpeg" + str(counter) + ".jpg"
+    file_number = 1
+    png_path = path + str(file_number) + ".png"
+    jpg_filename = temp_dir + "jpeg" + str(file_number) + ".jpg"
 
-    pdf_filename = "jpeg" + str(counter) + ".pdf"
+    pdf_filename = "jpeg" + str(file_number) + ".pdf"
 
     pdf_temp_dir = temp_dir + pdf_filename
 
@@ -78,13 +109,12 @@ def createPdfFile():
     os.replace(pdf_temp_dir, dest_dir + pdf_filename)
     shutil.rmtree(temp_dir)
 
-    # im1.save(pdf1_filename, "PDF" ,resolution=100.0, save_all=True, append_images=im_list)
     print("PDF file created")
 
 def on_press(key):
     if key == kb.Key.esc: os._exit(0)
-    if key == kb.Key.cmd: screenshooter()
-    if key == kb.Key.shift: createPdfFile()
+    if key == kb.Key.cmd: screenshooter(counter)
+    if key == kb.Key.shift: createPdfFile(counter)
     # if key == kb.Key.shift: listdir_nohidden('/Users/pawel.szalawinski/Pictures/Shoots/')
     if key == kb.Key.space: getlistdir_nohidden('/Users/pawel.szalawinski/Pictures/Shoots/')
 
---
+++ utils/sleeper.py
@@ -0,0 +1,10 @@
+import time 
+
+def printDots():
+    time.sleep(0.4)
+    print(".")
+    time.sleep(0.4)
+    print(".")
+    time.sleep(0.4)
+    print(".")
+    time.sleep(0.4)
