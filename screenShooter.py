ba1f13c9b25b018269269543564fe1234423e215
Pawel Szalawinski <pawel.szalawinski@tink.com>
2021-05-09 11:39:44 +0200

    refactoring

--- screenShooter.py
+++ screenShooter.py
@@ -10,15 +10,15 @@ from pynput import mouse
 class MyException(Exception): pass
 
 
-print("Keys:\n Cmd - takes screenshot\n Right ctrl - end program\n Shift - creates pdf from taken shots")
+print("Controls: Keys:\n Cmd - takes screenshot\n "
+      "Right ctrl - ends program\n Shift - creates pdf from taken shots"
+      " \nAvailable after providing necessary data")
 
 keyboard = kb.Controller()
 
 path = input("provide path where operations should be made: ")
-#  TODO temporary - to delete
-path = "/Users/pawel.szalawinski/Pictures/Shoots/"
 
-print(">>Path recorded")
+print(">>Path recorded: " + str(path))
 print("size of your main screen: ")
 print(pyautogui.size())
 
@@ -28,22 +28,14 @@ NumberOfMouseClicks = 0
 coordinates = list()
 
 
-def on_click(x, y, button, released):
+def on_click(x, y, button, pressed):
     global NumberOfMouseClicks
-    print(x, y)
-    # coordinates.append(x)
-    # coordinates.append(y)
 
     coordinates.append(pyautogui.position().x)
     coordinates.append(pyautogui.position().y)
 
-
-
-    print(pyautogui.position().x)
-    print(pyautogui.position().y)
-
     NumberOfMouseClicks += 1
-    if (NumberOfMouseClicks == 4):
+    if NumberOfMouseClicks == 4:
         raise MyException(button)
 
 
@@ -53,38 +45,32 @@ with mouse.Listener(on_click=on_click) as listener:
     except MyException as e:
         pass
 
-print("Coordinates: " + str(coordinates))
-sleeper.print_dots()
-#  TODO clean this mess
-licznik = counter.Counter(0, path, coordinates[0], coordinates[1], coordinates[4], coordinates[5])
+counterHolder = counter.Counter(0, path)
 positions = pyguiposition.ClickPositions(coordinates[0], coordinates[1], coordinates[4], coordinates[5])
-
-print(licznik.__dict__)
+sleeper.print_dots()
+print("Positions provided and saved")
 
 print("Ready to taking shots!")
 
 
-def key_shooter(x, y, button, pressed):
-    screen_shooter()
-
-
 def add_counter(obj):
-    licznik = obj.get_licz()
-    counter2 = licznik + 1
+    counterHolder = obj.get_licz()
+    counter2 = counterHolder + 1
     obj.set_licz(counter2)
 
 
 def screen_shooter(licznik):
-    # t = time.localtime()
-    # timestamp = time.strftime('%b-%d-%Y_%H%M%S', t)
-    click = licznik.get_licz()
+    click = counterHolder.get_licz()
     file_name = (str(click) + ".png")
     print("Shot taken: " + file_name)
-    path = licznik.get_pth()
+    path = counterHolder.get_pth()
     savepath = path + file_name
 
     myScreenshot = pyautogui.screenshot(
-        region=(int(positions.get_pos1x()), int(positions.get_pos1y()), int(positions.get_pos2x())-int(positions.get_pos1x()), int(positions.get_pos2y())-int(positions.get_pos1y())))
+        region=(
+            int(positions.get_pos1x()), int(positions.get_pos1y()),
+            int(positions.get_pos2x()) - int(positions.get_pos1x()),
+            int(positions.get_pos2y()) - int(positions.get_pos1y())))
 
     myScreenshot.save(savepath)
     add_counter(licznik)
@@ -98,9 +84,8 @@ def exit():
 
 def on_press(key):
     if key == kb.Key.ctrl: exit()
-    if key == kb.Key.cmd: screen_shooter(licznik)
-    if key == kb.Key.shift: merger.createPdfFile(licznik)
-    # if key == kb.Key.ctrl.a: listdir_nohidden('/Users/pawel.szalawinski/Pictures/Shoots/')
+    if key == kb.Key.cmd: screen_shooter(counterHolder)
+    if key == kb.Key.shift: merger.createPdfFile(counterHolder)
 
 
 with kb.Listener(
